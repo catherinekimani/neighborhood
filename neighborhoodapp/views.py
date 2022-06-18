17a9c5ca6cc7b08692162081b5b71b0c61c5d398
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-18 03:33:17 +0300

    Ft: Create signup view function & signup url

--- neighborhoodapp/views.py
+++ neighborhoodapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .forms import SignupForm
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.models import User
 
 # Create your views here.
+def signup(request):
+    form = SignupForm()
+    
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    return render(request,'accounts/signup.html',{'form':form})
