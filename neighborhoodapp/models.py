eb8122b2f9de67105c93db0e12ee8ca1e359c187
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-18 23:56:56 +0300

    Ft: Create profile model class

--- neighborhoodapp/models.py
+++ neighborhoodapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_bio = models.TextField(max_length=150)
+    user_contact = models.EmailField(max_length=100)
+    user_profile = CloudinaryField('image')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
