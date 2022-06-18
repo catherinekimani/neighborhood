86ee681de4e7ef36b3b625071fe18d91c1745bb6
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-18 03:59:09 +0300

    Ft: Create sign in view function & sign in url

--- neighborhoodapp/views.py
+++ neighborhoodapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 
-from .forms import SignupForm
+from .forms import SignupForm,SigninForm
 
 from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import login,logout,authenticate
 
 from django.contrib.auth.models import User
 
@@ -16,3 +18,17 @@ def signup(request):
             form.save()
             
     return render(request,'accounts/signup.html',{'form':form})
+
+def signin(request):
+    form = SigninForm()
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username = username, password = password)
+        
+        if user is not None:
+            login(request,user)
+            
+    return render(request,'accounts/signin.html',{'form':form})
