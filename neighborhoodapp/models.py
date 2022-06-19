960d843610da81ab898d99e14fb517a98b7b242d
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-19 12:04:38 +0300

    Ft: Create neighborhood model class

--- neighborhoodapp/models.py
+++ neighborhoodapp/models.py
@@ -13,3 +13,31 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+class Neighborhood(models.Model):
+    admin = models.ForeignKey(User,on_delete=models.CASCADE)
+    hood_name = models.CharField(max_length=100)
+    hood_location = models.CharField(max_length=100)
+    hood_descr = models.CharField(max_length=100)
+    hood_image = CloudinaryField('hood')
+    occupants_count = models.IntegerField(null=True, blank=True)
+    hospital_tel = models.IntegerField(null=True, blank=True)
+    police_count = models.IntegerField(null=True,blank=True )
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.hood_name}'
+    
+    def create_neigborhood(self):
+        self.save()
+        
+    def delete_neigborhood(self):
+        self.delete()
+    
+    def update_neighborhood(self):
+        self.save()
+        
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+    
